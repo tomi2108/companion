1d8b6f76de3976ca9284b3cde3a4bedd0e727fa3
tosanche <tomas.sancheznegrette@emeal.nttdata.com>
2025-06-09 09:43:47 -0300

    upgrade

--- src/cli/companion/upgrade.cjs
+++ src/cli/companion/upgrade.cjs
@@ -2,14 +2,15 @@
 
 const { executeScript } = require("../../interface/cmd.cjs");
 const path = require("node:path");
+const { pull } = require("../../interface/git.cjs");
 
 module.exports = {
   command: "upgrade",
   aliases: [],
   describe: "Upgrade companion",
   handler: async () => {
-
-    executeScript("install.sh", { path: path.resolve(__dirname, "../../../") });
-
+    const full_path = path.resolve(__dirname, "../../../");
+    await pull(full_path);
+    executeScript("install.sh", { path: full_path });
   }
 };
