06bf687dd18cd6ff5668ee51c912a542c0b97e07
tosanche <tomas.sancheznegrette@emeal.nttdata.com>
2025-06-17 17:53:55 -0300

    update deployments on pull

--- src/interface/files/deploy_repo.ts
+++ src/interface/files/deploy_repo.ts
@@ -5,24 +5,24 @@ import { DeployYaml } from "./deploy_yaml";
 import { Repo } from "./repo";
 
 export class DeployRepo extends Repo {
-  deployments!: DeployYaml[];
+  deployments: DeployYaml[];
 
   constructor(full_path: string) {
     super(full_path);
-    this.updateDeployments(full_path);
+    this.deployments = this.updateDeployments(full_path);
   }
 
-  private async updateDeployments(full_path: string) {
+  private updateDeployments(full_path: string) {
     const dir = fs.readdirSync(full_path, { withFileTypes: true });
     const yaml_files = dir
       .filter(DeployYaml.isDeployYamlFile)
       .map((f) => path.join(full_path, f.name));
-    this.deployments = yaml_files.map((f) => new DeployYaml(f));
+    return yaml_files.map((f) => new DeployYaml(f));
   }
 
   async pull(branch: string) {
-    const res = super.pull(branch);
-    this.updateDeployments(this.full_path);
+    const res = await super.pull(branch);
+    this.deployments = this.updateDeployments(this.full_path);
     return res;
   }
 
