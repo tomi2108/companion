dbc2658c0b07caa1ca465e39e4ac29a4802cf53f
tosanche <tomas.sancheznegrette@emeal.nttdata.com>
2025-08-25 16:28:22 -0300

    feat(app pipe): add server choice

--- cli/companion/pipeline.ts
+++ cli/companion/pipeline.ts
@@ -1,14 +1,18 @@
 import { promptForOcResource } from "@interface/prompts";
+import { search } from "@lib/ui";
 import { Openshift } from "@oc";
 import { getOcToken } from "@oc/api";
+
+const servers = ["brc", "cuyo"] as const;
 
 export default {
   command: "pipeline",
   aliases: ["pipe", "pipes", "pipe-log"],
   describe: "View pipelines logs",
   handler: async () => {
-    const token = await getOcToken("brc");
-    const projects = await new Openshift(token, "brc").getProjects();
+    const server = (await search({ choices: [...servers], message: "Choose server" })) as typeof servers[number];
+    const token = await getOcToken(server);
+    const projects = await new Openshift(token, server).getProjects();
 
     const project = await promptForOcResource(projects);
     const pipelines = await project.getPipelineRuns();
