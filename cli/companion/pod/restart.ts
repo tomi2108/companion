6d931dabfaf2b9d65e2599246db2c83ccc28cf21
tosanche <tomas.sancheznegrette@emeal.nttdata.com>
2025-09-18 18:20:00 -0300

    feat(pods restart): all

--- cli/companion/pod/restart.ts
+++ cli/companion/pod/restart.ts
@@ -2,7 +2,8 @@ import { Argv } from "yargs";
 
 import { promptForOcResource } from "@interface/prompts";
 import { Openshift } from "@oc";
-import { getOcToken } from "@oc/api";
+import { filterFrontendDeployments, getOcToken } from "@oc/api";
+import { Deployment } from "@oc/deployment";
 
 export default {
   command: "restart",
@@ -11,8 +12,28 @@ export default {
   builder: (yargs: Argv) => yargs
     .boolean("secret")
     .alias("secret", ["s"])
-    .describe("secret", "Restart all deployments affected by a secret"),
-  handler: async ({ secret }: { secret?: boolean }) => {
+    .describe("secret", "Restart all deployments affected by a secret")
+    .boolean("all")
+    .alias("all", ["a"])
+    .describe("all", "Whether to run the script for all repositories")
+    .boolean("frontend")
+    .alias("frontend", ["f"])
+    .describe("frontend", "Whether to run the script for all frontend repositories")
+    .boolean("backend")
+    .alias("backend", ["b"])
+    .describe("backend", "Whether to run the script for all backend repositories")
+    .conflicts("all", ["frontend", "backend"]),
+  handler: async ({
+    secret,
+    all,
+    frontend,
+    backend
+  }: {
+    secret?: boolean;
+    all?: boolean;
+    frontend?: boolean;
+    backend?: boolean;
+  }) => {
     const token = await getOcToken();
     const projects = await new Openshift(token).getProjects();
     const project = await promptForOcResource(projects);
@@ -28,7 +49,14 @@ export default {
       return;
     }
 
-    const deployment = await promptForOcResource(deployments);
-    await deployment.restart();
+    let to_restart: Deployment[] = [];
+    if (all || frontend) to_restart = [...to_restart, ...deployments.filter(filterFrontendDeployments)];
+    if (all || backend) to_restart = [...to_restart, ...deployments.filter((d) => !filterFrontendDeployments(d))];
+
+    if (to_restart.length === 0) {
+      const deployment = await promptForOcResource(deployments);
+      if (deployment) to_restart = [deployment];
+    }
+    await Promise.all(to_restart.map((d) => d.restart()));
   }
 };
