61afd53f94b44c4efdef89832f6abf2257c66baa
tosanche <tomas.sancheznegrette@emeal.nttdata.com>
2025-11-28 09:35:15 -0300

    feat(app status): show missing deployments

--- cli/companion/app/status.ts
+++ cli/companion/app/status.ts
@@ -1,12 +1,14 @@
 import chalk from "chalk";
 import Table from "cli-table3";
+import path from "node:path";
 
 import { getApp } from "@files";
+import { AppRepo } from "@files/app_repo";
 import { promptForOcResource } from "@interface/prompts";
 import { SonarQube } from "@interface/sonar/api";
 import { Config } from "@lib/config";
-import log from "@lib/log";
 import { loading } from "@lib/ui";
+import { readdirs } from "@lib/utils";
 import { Openshift } from "@oc";
 import { getOcToken } from "@oc/api";
 
@@ -20,7 +22,10 @@ export default {
     const token = await getOcToken();
     const projects = await new Openshift(token).getProjects();
     const project = await promptForOcResource(projects);
-    const deployments = await project.getDeployments();
+    const apps = [
+      ...readdirs(Config.get().paths.frontend) ?? [],
+      ...readdirs(Config.get().paths.backend) ?? []
+    ];
 
     const table = new Table({
       style: {
@@ -30,53 +35,69 @@ export default {
         "App",
         "Current version",
         "Last version",
+        "Updated",
         "Mocked",
         "TODOS"
       ]
     });
 
     const spinner = loading("Gathering status");
-    for (let index = 0; index < deployments.length; index += 20) {
-      const toProcess = deployments.slice(index, index + 20);
-      await Promise.all(toProcess.map(async (deployment) => {
-        const secrets = deployment.getSecrets() ?? [];
-        const { app_repo, deploy_repo } = await getApp(deployment.name);
-        if (!app_repo) {
-          log.warning(`Could not find app repo for ${deployment.name}`);
-          return;
-        }
-        if (!deploy_repo) {
-          log.warning(`Could not find deploy repo for ${deployment.name}`);
-          return;
-        }
-        const [last, current] = await Promise.all([
-          (async () => {
-            // TODO: for frontend repos look at -beta... -rc based on project
-            await app_repo.update();
-            return (await app_repo.getTags())[0];
-          })(),
-          (async () => {
-            await deploy_repo.update();
-            const deployment_file = deploy_repo.getDeployment(project.name);
-            if (!deployment_file) {
-              log.warning(`Could not find deployment file for ${deployment.name} and namespace ${project.name}`);
-              return;
-            }
-            return deployment_file.getVersion();
-          })()
-        ]);
-        if (!last) return;
+    for (let index = 0; index < apps.length; index += 20) {
+      const toProcess = apps.slice(index, index + 20);
+      await Promise.all(
+        toProcess.map(async (dir) => {
+          const app_repo = new AppRepo(path.join(dir.parentPath, dir.name));
+          const info = await app_repo.getInfo();
+          const deployment = await project.getDeployment(info.name).catch(() => null);
+          const secrets = deployment?.getSecrets() ?? null;
+          const [last, current] = await Promise.all([
+            (async () => {
+              // TODO: for frontend repos look at -beta... -rc based on project
+              await app_repo.update();
+              return (await app_repo.getTags())?.[0];
+            })(),
+            (async () => {
+              if (!deployment) return;
+              const { deploy_repo } = await getApp(info.name);
+              if (!deploy_repo) return;
+              await deploy_repo.update();
+              const deployment_file = deploy_repo.getDeployment(project.name);
+              if (!deployment_file) return;
+              return deployment_file.getVersion();
+            })()
+          ]);
+          if (!last) return;
 
-        const sonar_project = sonar_projects.find((p) => p.key.includes(deployment.name));
-        const todos = (await sonar.getCodeSmells(sonar_project.key)).filter((i) => i.message.includes("TODO"));
+          const sonar_project = sonar_projects.find((p) => p.key.includes(info.name));
+          const todos = (await sonar.getCodeSmells(sonar_project.key)).filter((i) => i.message.includes("TODO"));
 
-        const color = current === last ? "green" : "red";
-        const current_version = chalk[color](current);
-        const last_version = chalk[color](last);
-        const mockSecrets = Config.get().openshift.mock_secrets ?? [];
-        const mocked = secrets.some((s) => mockSecrets.includes(s.name)) ? chalk.green("yes") : chalk.red("no");
-        table.push([deployment.name, current_version, last_version, mocked, todos.length]);
-      }));
+          const isUpdated = current && last ? current === last : null;
+          const color = isUpdated ? "green" : "red";
+          const current_version = current ? chalk[color](current) : chalk.yellow("-");
+          const last_version = last ? chalk[color](last) : chalk.yellow("-");
+          const updated = (() => {
+            if (isUpdated === null) return chalk.yellow("-");
+            if (isUpdated) return chalk.green("yes");
+            return chalk.red("no");
+          })();
+
+          const mockSecrets = Config.get().openshift.mock_secrets ?? [];
+
+          const mocked = (() => {
+            if (secrets === null) return chalk.yellow("-");
+            if (secrets.some((s) => mockSecrets.includes(s.name))) return chalk.green("yes");
+            return chalk.red("no");
+          })();
+
+          table.push([
+            info.name,
+            current_version,
+            last_version,
+            updated,
+            mocked,
+            todos.length
+          ]);
+        }));
     }
     spinner.succeed();
     const sortedTable = table.sort((a, b) => {
