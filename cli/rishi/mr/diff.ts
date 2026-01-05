4f3ed3512487d36ab67786a041d5e5fcbb559c57
tosanche <tomas.sancheznegrette@emeal.nttdata.com>
2026-01-05 20:21:39 -0300

    mr diff

--- cli/rishi/mr/diff.ts
+++ cli/rishi/mr/diff.ts
@@ -1,21 +1,17 @@
-import { Dir } from "@files/dir";
-import { git } from "@glab/api";
-import { Repo } from "@interface/dirs/repo";
-import { promptForMr } from "@interface/prompts";
-import log from "@lib/log/default";
-import { getCurrentPath } from "@lib/utils";
+import { ExecutionContext } from "@lib/ctx";
+import { DiffMr } from "@workflow/steps/mr/DiffMr";
+import { PromptMr } from "@workflow/steps/mr/PromptMr";
+import { Workflow } from "@workflow/workflow";
 
 export default {
   command: "diff",
   aliases: [],
   describe: "View merge request diff",
   handler: async () => {
-    const dir = new Dir(getCurrentPath());
-    const repo = new Repo(dir);
-    const mr = await promptForMr(repo);
-    if (!mr.source_branch) return log.error("Could not find source_branch");
-    if (!mr.target_branch) return log.error("Could not find target_branch");
-    const diff = await git(dir).diff(["--color", mr.target_branch, mr.source_branch]);
-    log.info(diff);
+    const ctx = ExecutionContext.get();
+    await new Workflow([
+      new PromptMr(),
+      new DiffMr()
+    ]).run(ctx);
   }
 };
