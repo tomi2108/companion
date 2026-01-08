65fbe6e6381e92d87a5d89100ca718f191ca3e64
tosanche <tomas.sancheznegrette@emeal.nttdata.com>
2026-01-08 08:22:23 -0300

    mr create

--- cli/rishi/mr/create.ts
+++ cli/rishi/mr/create.ts
@@ -1,10 +1,5 @@
-import { AppRepo } from "@interface/dirs/app_repo";
 import { Repo } from "@interface/dirs/repo";
-import { promptChoice } from "@interface/prompts";
 import { ExecutionContext } from "@lib/ctx";
-import { confirm, loading } from "@lib/ui";
-import { Openshift } from "@oc";
-import { getOcToken } from "@oc/api";
 import { If } from "@steps/flow/If";
 import { Sleep } from "@steps/flow/Sleep";
 import { Write } from "@steps/flow/Write";
@@ -29,7 +24,7 @@ export default {
       }),
       new PromptBranch(),
       new If({
-        condition: () => confirm({ message: "Merge?" }),
+        condition: () => ctx.ui.confirm({ message: "Merge?" }),
         then: new Workflow([
           new CreateMr(),
           new Sleep({ seconds: 60 }),
@@ -38,39 +33,41 @@ export default {
       })
     ]).run(ctx);
 
-    const { name } = await repo.getInfo();
-    const { deploy_repo, app_repo } = await getApp(name);
-    if (merge && AppRepo.isAppRepo(dir)) {
-      deploys = await confirm({ message: "Deploy?" });
-      if (deploys) {
-        const token = await getOcToken();
-        const projects = await new Openshift(token).getProjects();
-        deploy_projects = await promptChoice(projects, { message: "Choose projects", multiple: true });
-      }
-    }
-
-    if (!merge) {
-      const spinner = loading("Building merge request");
-      const mr = await repo.createMr(targetBranch, { reviewer: add_reviewer ? default_reviewer : undefined });
-      spinner.succeed();
-      const open = await confirm({ message: `Open ${mr.title} in browser?` });
-      if (open) return mr.openInBrowser();
-    }
-
-    await repo.createAndMergeMr(targetBranch);
-
-    if (!deploys || !app_repo || !deploy_repo || deploy_projects?.length === 0) return;
-
-    const pipeline = await findCIPipeline(app_repo);
-    if (!pipeline) return log.error("Could not find ci pipeline");
-    const status = await waitForPipeline(pipeline, "Running CI pipeline");
-    if (status === PipelineStatus.failed) process.exit(1);
-
-    await app_repo?.update();
-    const tags = await app_repo.getTags({ sortByLastCreated: true });
-    const last_version = tags?.[0];
-    if (!tags || !last_version) return log.error("Could not find version to deploy");
-
-    deploy_repo.deploy(deploy_projects.map((p) => ({ configmaps: [], secrets: [], name: p.name })), last_version);
+    // TODO: maybe when waiting for pipelines works properly we can implement this
+    //   const { name } = await repo.getInfo();
+    //   const { deploy_repo, app_repo } = await getApp(name);
+    //   if (merge && AppRepo.isAppRepo(dir)) {
+    //     deploys = await confirm({ message: "Deploy?" });
+    //     if (deploys) {
+    //       const token = await getOcToken();
+    //       const projects = await new Openshift(token).getProjects();
+    //       deploy_projects = await promptChoice(projects, { message: "Choose projects", multiple: true });
+    //     }
+    //   }
+    //
+    //   if (!merge) {
+    //     const spinner = loading("Building merge request");
+    //     const mr = await repo.createMr(targetBranch, { reviewer: add_reviewer ? default_reviewer : undefined });
+    //     spinner.succeed();
+    //     const open = await confirm({ message: `Open ${mr.title} in browser?` });
+    //     if (open) return mr.openInBrowser();
+    //   }
+    //
+    //   await repo.createAndMergeMr(targetBranch);
+    //
+    //   if (!deploys || !app_repo || !deploy_repo || deploy_projects?.length === 0) return;
+    //
+    //   const pipeline = await findCIPipeline(app_repo);
+    //   if (!pipeline) return log.error("Could not find ci pipeline");
+    //   const status = await waitForPipeline(pipeline, "Running CI pipeline");
+    //   if (status === PipelineStatus.failed) process.exit(1);
+    //
+    //   await app_repo?.update();
+    //   const tags = await app_repo.getTags({ sortByLastCreated: true });
+    //   const last_version = tags?.[0];
+    //   if (!tags || !last_version) return log.error("Could not find version to deploy");
+    //
+    //   deploy_repo.deploy(deploy_projects.map((p) => ({ configmaps: [], secrets: [], name: p.name })), last_version);
+    // }
   }
 };
